2906728cfd91bc2173de5268b36777831edf10b4
DenielGuilhen <deniel.guilhen22@gmail.com>
2022-01-27 23:54:48 -0300

    app dio

---
+++ app.dio/Classes/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.dio.Interface;
+
+namespace app.dio.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+	{
+        private List<Serie> listaSerie = new List<Serie>();
+		public void Atualiza(int id, Serie objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaSerie[id].Excluir();
+		}
+
+		public void Insere(Serie objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Serie> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Serie RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+	}
+}
